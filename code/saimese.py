ca1cf5986e1d185ad7353d3eaef79f3b03e31117
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-11-25 10:56:22 +1100

    squashing bugs

--- code/saimese.py
+++ code/saimese.py
@@ -8,32 +8,35 @@ Created on Sat Nov 20 16:08:56 2021
 from imports import *
 from util_funcs import *
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-defaults.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if __name__ == '__main__':
 
-seed = 30
-np.random.seed(seed)
-torch.manual_seed(seed)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    defaults.device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu")
 
-# stim_path = Path(r'D:\Andrea_NN\stimuli\no_transf')
-# stim_path = Path(r'D:\Andrea_NN\stimuli\samediff')
-stim_path = Path('../samediff_no-transf')
-epochs = 1
-cycles = 1
-batch_sz = 24
-lr_min = 1e-4
-weight_decay = 1e-3
-w_dropout_1 = 0.8
-w_dropout_2 = 0.8
+    seed = 30
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
-p = (stim_path, epochs, cycles, batch_sz, lr_min, weight_decay, w_dropout_1,
-     w_dropout_2, seed)
+    # stim_path = Path(r'D:\Andrea_NN\stimuli\no_transf')
+    # stim_path = Path(r'D:\Andrea_NN\stimuli\samediff')
+    stim_path = Path('../samediff_no-transf')
+    epochs = 1
+    cycles = 1
+    batch_sz = 24
+    lr_min = 1e-4
+    weight_decay = 1e-3
+    w_dropout_1 = 0.8
+    w_dropout_2 = 0.8
 
-train_nets(p)
-# test_nets_noise()
+    p = (stim_path, epochs, cycles, batch_sz, lr_min, weight_decay,
+         w_dropout_1, w_dropout_2, seed)
 
-# TODO:
-# add model name to classes
-# Different feedback architectures
-# - fully interconnected vs conv
-# Deep net to fMRI mapping
+    train_nets(p)
+    # test_nets_noise()
+
+    # TODO:
+    # add model name to classes
+    # Different feedback architectures
+    # - fully interconnected vs conv
+    # Deep net to fMRI mapping
