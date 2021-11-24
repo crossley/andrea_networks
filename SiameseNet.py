4ed28d4990bf65d28751a43adaef4ea1d86ef49a
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-11-25 10:16:50 +1100

    refactor

--- SiameseNet.py
+++ SiameseNet.py
@@ -7,108 +7,47 @@ Created on Sun Nov 21 10:10:51 2021
 
 from imports import *
 
-class SiameseNet(nn.Module):
-    def __init__(self, w_dropout_1, w_dropout_2, forward_ind):
-        super(SiameseNet, self).__init__()
-        
-        self.forward_ind = forward_ind
 
-        # V1 layers
-        self.V1_p = nn.Sequential(
+class SiameseNet(nn.Module):
+    def __init__(self, w_dropout_1, w_dropout_2, head_mult):
+        super(SiameseNet, self).__init__()
+
+        self.V1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=7 // 2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-        # V2 layers
-        self.V2_p = nn.Sequential(
+        self.V2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=3 // 2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-        # V4 layers
-        self.V4_p = nn.Sequential(
+        self.V4 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=3 // 2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-        # IT layers
-        self.IT_p = nn.Sequential(
+        self.IT = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=3 // 2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-        # V1 layers
-        self.V1_f = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2,
-                      padding=7 // 2),  # + self.vfb,
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-
-        # V2 layers
-        self.V2_f = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=3 // 2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-
-        # V4 layers
-        self.V4_f = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=3 // 2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-
-        # IT layers
-        self.IT_f = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=3 // 2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-        
-        # feedback from periphery to fovea: IT to v1
-        self.fb = nn.Sequential(
-            nn.Conv2d(1024, 3, kernel_size=3, stride=1, padding=221),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-        
-        # feedback from periphery to fovea: v1 to v1
-        # TODO: padding probably needs fixing
-        self.fb2 = nn.Sequential(
-            nn.Conv2d(128, 3, kernel_size=3, stride=1, padding=197),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-
-        # head
-        if forward_ind == 1:
-            batch_mult = 2
-        elif forward_ind == 2:
-            batch_mult = 3
-        elif forward_ind == 3:
-            batch_mult = 2
-        elif forward_ind == 12:
-            batch_mult = 2
-        elif forward_ind == 22:
-            batch_mult = 3
-        elif forward_ind == 32:
-            batch_mult = 2
-            
         self.head = nn.Sequential(
             AdaptiveConcatPool2d(),
             nn.Flatten(),
-            nn.BatchNorm1d(1024 * batch_mult,
+            nn.BatchNorm1d(1024 * head_mult,
                            eps=1e-05,
                            momentum=0.1,
                            affine=True,
                            track_running_stats=True),
             nn.Dropout(p=w_dropout_1, inplace=False),
-            nn.Linear(in_features=1024 * batch_mult, out_features=512, bias=False),
+            nn.Linear(in_features=1024 * head_mult,
+                      out_features=512,
+                      bias=False),
             nn.ReLU(inplace=True),
             nn.BatchNorm1d(512,
                            eps=1e-05,
@@ -118,227 +57,6 @@ class SiameseNet(nn.Module):
             nn.Dropout(p=w_dropout_2, inplace=False),
             nn.Linear(in_features=512, out_features=2, bias=False),
         )
-        
+
     def forward(self, inp):
-        
-        if self.forward_ind == 1:
-            out = self.forward_1(inp)
-            
-        if self.forward_ind == 2:
-            out = self.forward_2(inp)
-            
-        if self.forward_ind == 3:
-            out = self.forward_3(inp)
-            
-        if self.forward_ind == 12:
-            out = self.forward_12(inp)
-            
-        if self.forward_ind == 22:
-            out = self.forward_22(inp)
-            
-        if self.forward_ind == 32:
-            out = self.forward_32(inp)
-            
-        return out
-
-    def forward_1(self, inp):
-        
-        inp1 = inp[0]
-        inp2 = inp[1]
-        fov_inp = inp[2]
-
-        # perihperal 1
-        v1_p1 = self.V1_p(inp1)
-        v2_p1 = self.V2_p(v1_p1)
-        v4_p1 = self.V4_p(v2_p1)
-        vIT_p1 = self.IT_p(v4_p1)
-
-        # perihperal 1
-        v1_p2 = self.V1_p(inp2)
-        v2_p2 = self.V2_p(v1_p2)
-        v4_p2 = self.V4_p(v2_p2)
-        vIT_p2 = self.IT_p(v4_p2)
-
-        out_cat = torch.cat((vIT_p1, vIT_p2), 1)
-
-        fb = self.fb(out_cat)
-        
-        # fovea
-        v1_fov = self.V1_f(fb + fov_inp)
-        v2_fov = self.V2_f(v1_fov)
-        v4_fov = self.V4_f(v2_fov)
-        vIT_fov = self.IT_f(v4_fov)
-
-        out_all = torch.cat((vIT_p1, vIT_p2), 1)
-    
-        out = self.head(out_all)
-        
-        return out
-        
-    def forward_2(self, inp):
-        
-        inp1 = inp[0]
-        inp2 = inp[1]
-        fov_inp = inp[2]
-
-        # perihperal 1
-        v1_p1 = self.V1_p(inp1)
-        v2_p1 = self.V2_p(v1_p1)
-        v4_p1 = self.V4_p(v2_p1)
-        vIT_p1 = self.IT_p(v4_p1)
-
-        # perihperal 1
-        v1_p2 = self.V1_p(inp2)
-        v2_p2 = self.V2_p(v1_p2)
-        v4_p2 = self.V4_p(v2_p2)
-        vIT_p2 = self.IT_p(v4_p2)
-
-        out_cat = torch.cat((vIT_p1, vIT_p2), 1)
-
-        fb = self.fb(out_cat)
-        
-        # fovea
-        v1_fov = self.V1_f(fb + fov_inp)
-        v2_fov = self.V2_f(v1_fov)
-        v4_fov = self.V4_f(v2_fov)
-        vIT_fov = self.IT_f(v4_fov)
-
-        out_all = torch.cat((vIT_p1, vIT_p2, vIT_fov), 1)
-    
-        out = self.head(out_all)
-
-        return out
-    
-    def forward_3(self, inp):
-        
-        inp1 = inp[0]
-        inp2 = inp[1]
-        fov_inp = inp[2]
-
-        # perihperal 1
-        v1_p1 = self.V1_p(inp1)
-        v2_p1 = self.V2_p(v1_p1)
-        v4_p1 = self.V4_p(v2_p1)
-        vIT_p1 = self.IT_p(v4_p1)
-
-        # perihperal 1
-        v1_p2 = self.V1_p(inp2)
-        v2_p2 = self.V2_p(v1_p2)
-        v4_p2 = self.V4_p(v2_p2)
-        vIT_p2 = self.IT_p(v4_p2)
-
-        out_cat = torch.cat((vIT_p1, vIT_p2), 1)
-
-        fb = self.fb(out_cat)
-        
-        # fovea
-        v1_fov = self.V1_f(fb + fov_inp)
-        v2_fov = self.V2_f(v1_fov)
-        v4_fov = self.V4_f(v2_fov)
-        vIT_fov = self.IT_f(v4_fov)
-
-        out_all = torch.cat((vIT_p1 + vIT_fov, vIT_p2 + vIT_fov), 1)
-    
-        out = self.head(out_all)
-        
-    def forward_12(self, inp):
-        
-        inp1 = inp[0]
-        inp2 = inp[1]
-        fov_inp = inp[2]
-
-        # perihperal 1
-        v1_p1 = self.V1_p(inp1)
-        v2_p1 = self.V2_p(v1_p1)
-        v4_p1 = self.V4_p(v2_p1)
-        vIT_p1 = self.IT_p(v4_p1)
-
-        # perihperal 1
-        v1_p2 = self.V1_p(inp2)
-        v2_p2 = self.V2_p(v1_p2)
-        v4_p2 = self.V4_p(v2_p2)
-        vIT_p2 = self.IT_p(v4_p2)
-
-        out_cat = torch.cat((v1_p1, v1_p2), 1)
-
-        fb = self.fb2(out_cat)
-        
-        # fovea
-        v1_fov = self.V1_f(fb + fov_inp)
-        v2_fov = self.V2_f(v1_fov)
-        v4_fov = self.V4_f(v2_fov)
-        vIT_fov = self.IT_f(v4_fov)
-
-        out_all = torch.cat((vIT_p1, vIT_p2), 1)
-    
-        out = self.head(out_all)
-        
-        return out
-        
-    def forward_22(self, inp):
-        
-        inp1 = inp[0]
-        inp2 = inp[1]
-        fov_inp = inp[2]
-
-        # perihperal 1
-        v1_p1 = self.V1_p(inp1)
-        v2_p1 = self.V2_p(v1_p1)
-        v4_p1 = self.V4_p(v2_p1)
-        vIT_p1 = self.IT_p(v4_p1)
-
-        # perihperal 1
-        v1_p2 = self.V1_p(inp2)
-        v2_p2 = self.V2_p(v1_p2)
-        v4_p2 = self.V4_p(v2_p2)
-        vIT_p2 = self.IT_p(v4_p2)
-
-        out_cat = torch.cat((v1_p1, v1_p2), 1)
-
-        fb = self.fb2(out_cat)
-        
-        # fovea
-        v1_fov = self.V1_f(fb + fov_inp)
-        v2_fov = self.V2_f(v1_fov)
-        v4_fov = self.V4_f(v2_fov)
-        vIT_fov = self.IT_f(v4_fov)
-
-        out_all = torch.cat((vIT_p1, vIT_p2, vIT_fov), 1)
-    
-        out = self.head(out_all)
-
-        return out
-    
-    def forward_32(self, inp):
-        
-        inp1 = inp[0]
-        inp2 = inp[1]
-        fov_inp = inp[2]
-
-        # perihperal 1
-        v1_p1 = self.V1_p(inp1)
-        v2_p1 = self.V2_p(v1_p1)
-        v4_p1 = self.V4_p(v2_p1)
-        vIT_p1 = self.IT_p(v4_p1)
-
-        # perihperal 1
-        v1_p2 = self.V1_p(inp2)
-        v2_p2 = self.V2_p(v1_p2)
-        v4_p2 = self.V4_p(v2_p2)
-        vIT_p2 = self.IT_p(v4_p2)
-
-        out_cat = torch.cat((v1_p1, v1_p2), 1)
-
-        fb = self.fb2(out_cat)
-        
-        # fovea
-        v1_fov = self.V1_f(fb + fov_inp)
-        v2_fov = self.V2_f(v1_fov)
-        v4_fov = self.V4_f(v2_fov)
-        vIT_fov = self.IT_f(v4_fov)
-
-        out_all = torch.cat((vIT_p1 + vIT_fov, vIT_p2 + vIT_fov), 1)
-    
-        out = self.head(out_all)
-
-        return out
+        pass
--- util_funcs.py
+++ util_funcs.py
@@ -129,7 +129,7 @@ def get_y(t):
     return t[3]
 
 
-def make_dls(stim_path, batch_sz=24, fov_noise=False, seed=0, test_prop=0.2):
+def make_dls(stim_path, batch_sz=24, seed=0, test_prop=0.2):
 
     stim_path = Path(stim_path)
     pairs = glob.glob(os.path.join(stim_path, "*.png"))
@@ -142,22 +142,13 @@ def make_dls(stim_path, batch_sz=24, fov_noise=False, seed=0, test_prop=0.2):
                                  stratify=y)
     splits = splitter(fnames)
     # splits = RandomSplitter()(fnames)
-    if fov_noise:
-        siamese = DataBlock(
-            blocks=(ImageTupleBlock, CategoryBlock),
-            get_items=get_tuples_noise,
-            get_x=get_x,
-            get_y=get_y,
-            splitter=splitter,
-        )
-    else:
-        siamese = DataBlock(
-            blocks=(ImageTupleBlock, CategoryBlock),
-            get_items=get_tuples_no_noise,
-            get_x=get_x,
-            get_y=get_y,
-            splitter=splitter,
-        )
+    siamese = DataBlock(
+        blocks=(ImageTupleBlock, CategoryBlock),
+        get_items=get_tuples_no_noise,
+        get_x=get_x,
+        get_y=get_y,
+        splitter=splitter,
+    )
 
     dls = siamese.dataloaders(
         fnames,
@@ -166,6 +157,7 @@ def make_dls(stim_path, batch_sz=24, fov_noise=False, seed=0, test_prop=0.2):
         # shuffle=True,
         # device = 'cuda',
     )
+
     # check that train and test splits have balanced classes
     train_test = ["TRAIN", "TEST"]
     for train_test_id in [0, 1]:
@@ -325,18 +317,13 @@ def init_pretrained_weights(net):
     return net
 
 
-def train_net(p):
+def train_net(net, optimizer, p):
 
-    net = p[0]
-    optimizer = p[1]
-    criterion = p[2]
-    cycles = p[3]
-    epochs = p[4]
-    train_loader = p[5]
-    test_loader = p[6]
-
-    # start training
-    print("\nTrain/Test started!")
+    criterion = p[0]
+    cycles = p[1]
+    epochs = p[2]
+    train_loader = p[3]
+    test_loader = p[4]
 
     for cycle in range(cycles):
         tr_loss = []
@@ -388,7 +375,7 @@ def train_net(p):
                 te_acc.append(100 * te_correct / te_total)
                 te_loss.append(te_running_loss)
                 end = time.time() - start
-                log_msg = f"%5d / %5d TRAIN/TEST losses: \t %.8f \t %.8f \t\t acc: \t %.2f %% \t %.2f %% \t\t time: {round(end,3)}" % (
+                print(
                     cycle + 1,
                     epoch + 1,
                     tr_running_loss,
@@ -396,39 +383,33 @@ def train_net(p):
                     100 * tr_correct / tr_total,
                     100 * te_correct / te_total,
                 )
-                print(log_msg)
 
     return (tr_loss, tr_acc, te_loss, te_acc, cf_pred, cf_y)
 
 
-def test_net(p):
+def test_net(net, p):
 
-    net = p[0]
-    optimizer = p[1]
-    criterion = p[2]
-    cycles = p[3]
-    epochs = p[4]
-    train_loader = p[5]
-    test_loader = p[6]
-    noise_vars = p[7]
+    cycles = p[0]
+    epochs = p[1]
+    train_loader = p[2]
+    test_loader = p[3]
+    noise_vars = p[4]
 
-    print("\nTest started!")
-    
     res = []
-    
+
     for v in noise_vars:
 
         for cycle in range(cycles):
-    
+
             tr_loss = []
             tr_acc = []
             te_loss = []
             te_acc = []
-    
+
             for epoch in range(epochs):
-    
+
                 net.eval()
-    
+
                 te_running_loss = 0.0
                 te_correct = 0
                 te_total = 0
@@ -437,12 +418,11 @@ def test_net(p):
                 with torch.no_grad():
                     start = time.time()
                     for (inputs, labels) in train_loader:
-                    
-                        inputs = (inputs[0], 
-                                  inputs[1], 
-                                  inputs[2] + v * torch.randn(inputs[2].shape, 
-                                                          device='cuda'))
-            
+
+                        inputs = (
+                            inputs[0], inputs[1], inputs[2] +
+                            v * torch.randn(inputs[2].shape, device='cuda'))
+
                         out = net(inputs)
                         _, pred = torch.max(out, 1)
                         loss = criterion(out, labels)
@@ -451,47 +431,35 @@ def test_net(p):
                         te_correct += (pred == labels).sum().item()
                         cf_y += labels.cpu().detach().tolist()
                         cf_pred += pred.cpu().detach().tolist()
-    
+
                     te_acc.append(100 * te_correct / te_total)
                     te_loss.append(te_running_loss)
                     end = time.time() - start
-                    log_msg = f"%5d / %5d TRAIN/TEST losses: \t \t \t\t acc: \t %.2f %% \t %.2f %% \t\t time: {round(end,3)}" % (
-                        cycle + 1,
-                        epoch + 1,
-                        te_running_loss,
-                        100 * te_correct / te_total,
-                    )
-                    print(log_msg)
-                    
-        d = pd.DataFrame({'noise_var': v,
-                          'te_loss':te_loss,
-                          'te_acc': te_acc
-                          })
+                    print(cycle + 1, epoch + 1, te_running_loss,
+                          100 * te_correct / te_total)
+
+        d = pd.DataFrame({
+            'noise_var': v,
+            'te_loss': te_loss,
+            'te_acc': te_acc
+        })
         res.append(d)
-        
+
     res = pd.concat(res)
-                
+
     return res
 
 
 def inspect_results_test(res):
-    
-    res_test_1 = res[0]
-    res_test_2 = res[1]
-    res_test_3 = res[2]
-    
-    fig, ax = plt.subplots(1, 3, figsize=(12, 4), squeeze=False)
-    sn.violinplot(data=res_test_1, x='noise_var', y='te_acc', ax=ax[0, 0])
-    sn.violinplot(data=res_test_2, x='noise_var', y='te_acc', ax=ax[0, 1])
-    sn.violinplot(data=res_test_3, x='noise_var', y='te_acc', ax=ax[0, 2])
-    # ax[0, 0].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    # ax[0, 1].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    # ax[0, 2].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    # ax[0, 0].set_xticks(rotation='vertical')
-    # ax[0, 1].set_xticks(rotation='vertical')
-    # ax[0, 2].set_xticks(rotation='vertical')
+
+    fig, ax = plt.subplots(1, len(res), figsize=(12, 4), squeeze=False)
+
+    for i in range(len(res)):
+        sn.violinplot(data=res[i], x='noise_var', y='te_acc', ax=ax[0, i])
+
     plt.tight_layout()
     plt.show()
+
 
 def inspect_results(res):
 
@@ -501,19 +469,24 @@ def inspect_results(res):
     te_acc = res[3]
     cf_pred = res[4]
     cf_y = res[5]
-    
+
     fig, ax = plt.subplots(1, 3, figsize=(10, 4), squeeze=False)
-    
+
     cf_matrix = confusion_matrix(cf_y, cf_pred)
     df_cm = pd.DataFrame(
         cf_matrix,
         index=[i for i in ["Same", "Different"]],
         columns=[i for i in ["Same", "Different"]],
     )
-    sn.heatmap(df_cm, annot=True, cbar=False, cmap="Blues", fmt="d", ax=ax[0, 0])
+    sn.heatmap(df_cm,
+               annot=True,
+               cbar=False,
+               cmap="Blues",
+               fmt="d",
+               ax=ax[0, 0])
     ax[0, 0].set_xlabel("Predicted")
     ax[0, 0].set_ylabel("Actual")
-    
+
     ax[0, 1].plot(tr_loss, label="Train")
     ax[0, 1].plot(te_loss, label="Test")
     ax[0, 1].set_xlabel("Epoch")
@@ -523,13 +496,14 @@ def inspect_results(res):
     ax[0, 2].plot(te_acc, label="Test")
     ax[0, 2].set_xlabel("Epoch")
     ax[0, 2].set_ylabel("Accuracy")
-    
+
     plt.legend()
     plt.tight_layout()
     plt.show()
-    
+
+
 def train_nets(p):
-    
+
     stim_path = p[0]
     epochs = p[1]
     cycles = p[2]
@@ -538,217 +512,73 @@ def train_nets(p):
     weight_decay = p[5]
     w_dropout_1 = p[6]
     w_dropout_2 = p[7]
-    fov_noise = p[8]
-    seed = p[9]
+    seed = p[8]
 
-    dls = make_dls(stim_path, batch_sz, fov_noise, seed)
+    dls = make_dls(stim_path, batch_sz, seed)
     train_loader = dls.train
     test_loader = dls.valid
     # dls.show_batch(max_n = 2)
     # plt.show()
-    
-    net_1 = SiameseNet(w_dropout_1, w_dropout_2, 1)
-    net_2 = SiameseNet(w_dropout_1, w_dropout_2, 2)
-    net_3 = SiameseNet(w_dropout_1, w_dropout_2, 3)
-    
-    net_1 = init_weights(net_1)
-    net_2 = init_weights(net_2)
-    net_3 = init_weights(net_3)
-    
-    net_1 = init_pretrained_weights(net_1)
-    net_2 = init_pretrained_weights(net_2)
-    net_3 = init_pretrained_weights(net_3)
-    
-    criterion = nn.CrossEntropyLoss()
-    
-    params_to_update_1 = net_1.parameters()
-    params_to_update_2 = net_2.parameters()
-    params_to_update_3 = net_3.parameters()
-    
-    optimizer_1 = optim.Adam(filter(lambda p: p.requires_grad, params_to_update_1),
-                             lr=lr_min,
-                             weight_decay=weight_decay)
-    optimizer_2 = optim.Adam(filter(lambda p: p.requires_grad, params_to_update_2),
-                             lr=lr_min,
-                             weight_decay=weight_decay)
-    optimizer_3 = optim.Adam(filter(lambda p: p.requires_grad, params_to_update_3),
-                             lr=lr_min,
-                             weight_decay=weight_decay)
-    
-    net_1 = nn.DataParallel(net_1)
-    net_2 = nn.DataParallel(net_2)
-    net_3 = nn.DataParallel(net_3)
-    
-    net_1.to('cuda')
-    net_2.to('cuda')
-    net_3.to('cuda')
-    
-    p_1 = (net_1, optimizer_1, criterion, cycles, epochs, train_loader,
-           test_loader)
-    p_2 = (net_2, optimizer_2, criterion, cycles, epochs, train_loader,
-           test_loader)
-    p_3 = (net_3, optimizer_3, criterion, cycles, epochs, train_loader,
-           test_loader)
-    
-    res_1 = train_net(p_1)
-    res_2 = train_net(p_2)
-    res_3 = train_net(p_3)
-    
-    torch.save(net_1.state_dict(), 'net_1.pth')
-    torch.save(net_2.state_dict(), 'net_2.pth')
-    torch.save(net_3.state_dict(), 'net_3.pth')
-    
-    inspect_results(res_1)
-    inspect_results(res_2)
-    inspect_results(res_3)
-    
-# TODO: PICK UP HERE    
-def train_nets_2(p):
-    
-    stim_path = p[0]
-    epochs = p[1]
-    cycles = p[2]
-    batch_sz = p[3]
-    lr_min = p[4]
-    weight_decay = p[5]
-    w_dropout_1 = p[6]
-    w_dropout_2 = p[7]
-    fov_noise = p[8]
-    seed = p[9]
 
-    dls = make_dls(stim_path, batch_sz, fov_noise, seed)
-    train_loader = dls.train
-    test_loader = dls.valid
-    # dls.show_batch(max_n = 2)
-    # plt.show()
-    
-    net_1 = SiameseNet(w_dropout_1, w_dropout_2, 12)
-    net_2 = SiameseNet(w_dropout_1, w_dropout_2, 22)
-    net_3 = SiameseNet(w_dropout_1, w_dropout_2, 32)
-    
-    net_1 = init_weights(net_1)
-    net_2 = init_weights(net_2)
-    net_3 = init_weights(net_3)
-    
-    net_1 = init_pretrained_weights(net_1)
-    net_2 = init_pretrained_weights(net_2)
-    net_3 = init_pretrained_weights(net_3)
-    
+    net_0 = SiameseNet0(w_dropout_1, w_dropout_2)
+    net_1 = SiameseNet1(w_dropout_1, w_dropout_2)
+    net_2 = SiameseNet2(w_dropout_1, w_dropout_2)
+    net_3 = SiameseNet12(w_dropout_1, w_dropout_2)
+    net_4 = SiameseNet22(w_dropout_1, w_dropout_2)
+
+    nets = [net_0, net_1, net_2, net_3, net_4]
+
+    [init_weights(x) for x in nets]
+    [init_pretrained_weights(x) for x in nets]
+
+    params_to_update = [x.parameters() for x in nets]
+
+    optimizers = [
+        optim.Adam(filter(lambda p: p.requires_grad, x),
+                   lr=lr_min,
+                   weight_decay=weight_decay) for x in params_to_update
+    ]
+
+    nets = [nn.DataParallel(x) for x in nets]
+
+    [x.to('cuda') for x in nets]
+
     criterion = nn.CrossEntropyLoss()
-    
-    params_to_update_1 = net_1.parameters()
-    params_to_update_2 = net_2.parameters()
-    params_to_update_3 = net_3.parameters()
-    
-    optimizer_1 = optim.Adam(filter(lambda p: p.requires_grad, params_to_update_1),
-                             lr=lr_min,
-                             weight_decay=weight_decay)
-    optimizer_2 = optim.Adam(filter(lambda p: p.requires_grad, params_to_update_2),
-                             lr=lr_min,
-                             weight_decay=weight_decay)
-    optimizer_3 = optim.Adam(filter(lambda p: p.requires_grad, params_to_update_3),
-                             lr=lr_min,
-                             weight_decay=weight_decay)
-    
-    net_1 = nn.DataParallel(net_1)
-    net_2 = nn.DataParallel(net_2)
-    net_3 = nn.DataParallel(net_3)
-    
-    net_1.to('cuda')
-    net_2.to('cuda')
-    net_3.to('cuda')
-    
-    p_1 = (net_1, optimizer_1, criterion, cycles, epochs, train_loader,
-           test_loader)
-    p_2 = (net_2, optimizer_2, criterion, cycles, epochs, train_loader,
-           test_loader)
-    p_3 = (net_3, optimizer_3, criterion, cycles, epochs, train_loader,
-           test_loader)
-    
-    res_1 = train_net(p_1)
-    res_2 = train_net(p_2)
-    res_3 = train_net(p_3)
-    
-    torch.save(net_1.state_dict(), 'net_12.pth')
-    torch.save(net_2.state_dict(), 'net_22.pth')
-    torch.save(net_3.state_dict(), 'net_32.pth')
-    
-    inspect_results(res_1)
-    inspect_results(res_2)
-    inspect_results(res_3)
-    
+
+    p = (criterion, cycles, epochs, train_loader, test_loader)
+
+    res = [train_net(nets[x], optimizers[x], p) for x in range(len(nets))]
+
+    [torch.save(x.state_dict(), 'net_' + x.model_name + '.pth') for x in nets]
+
+    inspect_results(res)
+
+
 def test_nets_noise():
-    
-    net_1 = SiameseNet(w_dropout_1, w_dropout_2, 1)
-    net_2 = SiameseNet(w_dropout_1, w_dropout_2, 2)
-    net_3 = SiameseNet(w_dropout_1, w_dropout_2, 3)
-    
-    net_1 = nn.DataParallel(net_1)
-    net_2 = nn.DataParallel(net_2)
-    net_3 = nn.DataParallel(net_3)
-    
-    net_1.load_state_dict(torch.load('net_1.pth'))
-    net_2.load_state_dict(torch.load('net_2.pth'))
-    net_3.load_state_dict(torch.load('net_3.pth'))
-    
-    net_1.to('cuda')
-    net_2.to('cuda')
-    net_3.to('cuda')
-    
+
+    net_0 = SiameseNet0(w_dropout_1, w_dropout_2)
+    net_1 = SiameseNet1(w_dropout_1, w_dropout_2)
+    net_2 = SiameseNet2(w_dropout_1, w_dropout_2)
+    net_3 = SiameseNet12(w_dropout_1, w_dropout_2)
+    net_4 = SiameseNet22(w_dropout_1, w_dropout_2)
+
+    nets = [net_0, net_1, net_2, net_3, net_4]
+    nets = [nn.DataParallel(x) for x in nets]
+
+    [
+        x.load_state_dict(torch.load('net_' + x.model_name + '.pth'))
+        for x in nets
+    ]
+
+    [x.to('cuda') for x in nets]
+
     cycles = 1
     epochs = 1
     noise_levels = np.linspace(0.0, 3.0, 30)
     batch_sz = test_loader.n
-    fov_noise = False
-    
-    p_1 = (net_1, optimizer_1, criterion, cycles, epochs, train_loader,
-           test_loader, noise_levels)
-    p_2 = (net_2, optimizer_2, criterion, cycles, epochs, train_loader,
-           test_loader, noise_levels)
-    p_3 = (net_3, optimizer_3, criterion, cycles, epochs, train_loader,
-           test_loader, noise_levels)
-    
-    res_test_1 = test_net(p_1)
-    res_test_2 = test_net(p_2)
-    res_test_3 = test_net(p_3)
-    
-    res = (res_test_1, res_test_2, res_test_3)
-    inspect_results_test(res)
-    
-def test_nets_noise_2():
-    
-    net_1 = SiameseNet(w_dropout_1, w_dropout_2, 12)
-    net_2 = SiameseNet(w_dropout_1, w_dropout_2, 22)
-    net_3 = SiameseNet(w_dropout_1, w_dropout_2, 32)
-    
-    net_1 = nn.DataParallel(net_1)
-    net_2 = nn.DataParallel(net_2)
-    net_3 = nn.DataParallel(net_3)
-    
-    net_1.load_state_dict(torch.load('net_12.pth'))
-    net_2.load_state_dict(torch.load('net_22.pth'))
-    net_3.load_state_dict(torch.load('net_32.pth'))
-    
-    net_1.to('cuda')
-    net_2.to('cuda')
-    net_3.to('cuda')
-    
-    cycles = 1
-    epochs = 1
-    noise_levels = np.linspace(0.0, 3.0, 30)
-    batch_sz = test_loader.n
-    fov_noise = False
-    
-    p_1 = (net_1, optimizer_1, criterion, cycles, epochs, train_loader,
-           test_loader, noise_levels)
-    p_2 = (net_2, optimizer_2, criterion, cycles, epochs, train_loader,
-           test_loader, noise_levels)
-    p_3 = (net_3, optimizer_3, criterion, cycles, epochs, train_loader,
-           test_loader, noise_levels)
-    
-    res_test_1 = test_net(p_1)
-    res_test_2 = test_net(p_2)
-    res_test_3 = test_net(p_3)
-    
-    res = (res_test_1, res_test_2, res_test_3)
+
+    p = (cycles, epochs, train_loader, test_loader, noise_levels)
+
+    res = [test_net(x, p) for x in nets]
+
     inspect_results_test(res)
