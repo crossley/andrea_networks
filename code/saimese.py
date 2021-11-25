9e5a21b9becbedc75a206d9fa1094fe14ffd609a
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-11-25 14:03:13 +1100

    squashing bugs

--- code/saimese.py
+++ code/saimese.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     defaults.device = torch.device(
         "cuda" if torch.cuda.is_available() else "cpu")
 
+    print(device)
+
     seed = 30
     np.random.seed(seed)
     torch.manual_seed(seed)
